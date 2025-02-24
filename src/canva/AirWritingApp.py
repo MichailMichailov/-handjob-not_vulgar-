5d9d48f0484bd863f2096937066e0542335a74e2
Michael Starovoitov <mihatestform@gmail.com>
2025-02-24 14:49:24 +0200

    add fask

--- src/canva/AirWritingApp.py
+++ src/canva/AirWritingApp.py
@@ -11,8 +11,37 @@ class AirWritingApp:
         self.canvas = None
         self.writer = GestureWriter(train_data_folder)
 
-    def run(self):
-        while self.cap.isOpened():
+    # def run(self):
+    #     while self.cap.isOpened():
+    #         ret, frame = self.cap.read()
+    #         if not ret:
+    #             break
+    #         frame = cv2.flip(frame, 1)
+    #         h, w, _ = frame.shape
+    #         if self.canvas is None:
+    #             self.canvas = DrawingCanvas(w, h)
+    #         finger_pos = self.tracker.get_finger_position(frame)
+    #         if finger_pos:
+    #             if finger_pos == -1:
+    #                 self.canvas.clear_prev()
+    #             else:
+    #                 x, y = finger_pos
+    #                 self.canvas.draw_line(x, y)
+    #         frame[0:400, 0:400] = self.canvas.get_canvas()
+    #         rezult = self.writer.recognize_letter(self.canvas.get_single_letter())
+    #         self.tracker.write_text_on_canvas(frame, rezult, 400, 400)
+
+    #         cv2.imshow("Air Writing", frame)
+    #         key = cv2.waitKey(1)
+    #         if key == 27: # esc
+    #             break
+    #         elif key == 99: # c
+    #             self.canvas.clear()
+    #     self.cap.release()
+    #     cv2.destroyAllWindows()
+
+    def generate_frames(self):
+        while True:
             ret, frame = self.cap.read()
             if not ret:
                 break
@@ -20,6 +49,8 @@ class AirWritingApp:
             h, w, _ = frame.shape
             if self.canvas is None:
                 self.canvas = DrawingCanvas(w, h)
+            
+            # Получаем координаты пальца
             finger_pos = self.tracker.get_finger_position(frame)
             if finger_pos:
                 if finger_pos == -1:
@@ -27,15 +58,28 @@ class AirWritingApp:
                 else:
                     x, y = finger_pos
                     self.canvas.draw_line(x, y)
-            frame[0:400, 0:400] = self.canvas.get_canvas()
-            rezult = self.writer.recognize_letter(self.canvas.get_single_letter())
-            self.tracker.write_text_on_canvas(frame, rezult, 400, 400)
 
-            cv2.imshow("Air Writing", frame)
-            key = cv2.waitKey(1)
-            if key == 27: # esc
-                break
-            elif key == 99: # c
-                self.canvas.clear()
-        self.cap.release()
-        cv2.destroyAllWindows()
+            # Получаем изображение canvas
+            canvas_img = self.canvas.get_canvas()
+            
+            # Проверяем количество каналов (избегаем ошибки OpenCV)
+            if len(canvas_img.shape) == 2:
+                canvas_bgr = cv2.cvtColor(canvas_img, cv2.COLOR_GRAY2BGR)
+            else:
+                canvas_bgr = canvas_img
+            
+            # Встраиваем canvas в кадр камеры
+            frame[0:400, 0:400] = canvas_bgr  
+
+            # Распознаем букву
+            # result, confidence = self.writer.recognize_letter(self.canvas.get_single_letter())
+            # text = f"{result} ({confidence*100:.2f}%)"
+            text = 'ddd'
+            self.tracker.write_text_on_canvas(frame, text, 400, 400)
+
+            # Кодируем изображение в формат JPEG
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame_bytes = buffer.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
 
 from canva.AirWritingApp import AirWritingApp
-if __name__ == "__main__":
-    app = AirWritingApp()
-    app.run()
+from flask import Flask, Response, render_template
+# if __name__ == "__main__":
+#     app = AirWritingApp()
+#     app.run()
 
+app = Flask(__name__)
+app_instance = AirWritingApp()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(app_instance.generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/clear_canvas', methods=['POST'])
+def clear_canvas():
+    app_instance.canvas.clear()  # Очищаем холст
+    return '', 204  # Возвращаем пустой ответ
+
+@app.route('/get_letter', methods=['POST'])
+def get_letter():
+    print(app_instance.writer.recognize_letter(app_instance.canvas.get_single_letter()))
+    return '', 204  # Возвращаем пустой ответ
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
